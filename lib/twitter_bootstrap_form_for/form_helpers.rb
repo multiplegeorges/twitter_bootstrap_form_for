d9da31af792eb6f0bbeecd1162f5db66efce7b17
Georges Gabereau <georges.gabereau@gmail.com>
2012-06-05 21:49:59 -0400

    simplified the error proc override.

--- lib/twitter_bootstrap_form_for/form_helpers.rb
+++ lib/twitter_bootstrap_form_for/form_helpers.rb
@@ -3,28 +3,16 @@ require 'twitter_bootstrap_form_for'
 module TwitterBootstrapFormFor::FormHelpers
   [:form_for, :fields_for].each do |method|
     module_eval do
-      define_method "twitter_bootstrap_#{method}" do |record, *args, &block|
+      define_method "bootstrap_#{method}" do |record, *args, &block|
         # add the TwitterBootstrap builder to the options
         options           = args.extract_options!
         options[:builder] = TwitterBootstrapFormFor::FormBuilder
-
-        # call the original method with our overridden options
-        _override_field_error_proc do
-          send method, record, *(args << options), &block
-        end
       end
     end
   end
 
-  private
-
-  BLANK_FIELD_ERROR_PROC = lambda {|input, *_| input }
-
-  def _override_field_error_proc
-    original_field_error_proc           = ::ActionView::Base.field_error_proc
-    ::ActionView::Base.field_error_proc = BLANK_FIELD_ERROR_PROC
-    yield
-  ensure
-    ::ActionView::Base.field_error_proc = original_field_error_proc
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
+    "#{html_tag}".html_safe
   end
+  
 end
